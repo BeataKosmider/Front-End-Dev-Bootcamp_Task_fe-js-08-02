8138facb2f14e6ac91647551233411f9cb794b4c
Administrator <admin@example.com>
2020-07-09 06:52:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 class Fruit {
-  constructor(name, color, sweetness) {
-    this.name = name;
-    this.color = color;
-    this.sweetness = sweetness;
-  }
+
 };
 
 class Apple extends Fruit {
-  constructor(goodForCider, goodForJuice, ...args) {
-    super(...args)
-    this.goodForCider = goodForCider;
-    this.goodForJuice = goodForJuice;
-  }
+
 };
 
 module.exports = {
