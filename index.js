815c706c237f4b08ba41491657bead4cb3e8a34d
Beata Kosmider <kosmibe@gmail.com>
2023-02-22 10:54:51 +0100

    task with 3 tests passed

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 class Fruit {
   // kod tutaj
+  constructor(name, color, sweetness) {
+    this.name = "antonowka";
+    this.color = "red";
+    this.sweetness = 5;
+  }
 }
 
 class Apple extends Fruit {
   // oraz tutaj :)
+  constructor(goodForCider, goodForJuice, name, color, sweetness) {
+    super(name, color, sweetness);
+    this.goodForCider = true;
+    this.goodForJuice = false;
+  }
 }
 
 module.exports = {
